2ad386d4d3d1b0b6e9b1a31cbd7212807b60cd43
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-05-11 15:03:15 +0100

    config: hardhat networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    mainnet: {
+      accounts: [process.env.PRIVATE_KEY || ""],
+      url: process.env.RPC_URL,
+    },
+    goerli: {
+      accounts: [process.env.PRIVATE_KEY || ""],
+      url: process.env.RPC_URL,
+    },
+  },
 };
 
 export default config;
