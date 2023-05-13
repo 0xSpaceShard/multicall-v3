3dd4c019f5dc3756e60cbbb927a3894b44e50d55
shardAstronaut <127413534+shardAstronaut@users.noreply.github.com>
2023-05-13 16:02:18 +0100

    deploy: multicall to mainnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,31 @@
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
-async function main() {
-  const multicallFactory = await ethers.getContractFactory("Multicall");
-  const multicall = await multicallFactory.deploy();
-
-  await multicall.deployed();
-
-  console.log(`Multicall deployed to ${multicall.address}`);
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function main() {
+  while (true) {
+    console.log("Start")
+    const gasPrice = await ethers.provider.getGasPrice()
+    if (gasPrice.lte(BigNumber.from("40000000000"))){
+      console.log(`Start deployed`);
+      
+      const multicallFactory = await ethers.getContractFactory("Multicall");
+      const multicall = await multicallFactory.deploy();
+      
+      await multicall.deployed();
+    
+      console.log(`Multicall deployed to ${multicall.address}`);
+      return
+    } else {
+      console.log("Price too high", gasPrice.div(1000000000).toString())
+    }
+    await sleep(30000)
+  }
+}
+
 
 main().catch((error) => {
   console.error(error);
